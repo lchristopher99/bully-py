ed48ea445c1c25a4c1d09046c215e55f135f8a35
Brett Baltz <57783458+BrettBaltz@users.noreply.github.com>
2021-12-02 21:20:04 -0600

    Add files via upload

---
+++ final_version/cowbell.py
@@ -0,0 +1,81 @@
+from firebase import firebase
+
+db_url = "https://groupproject-8d2e6-default-rtdb.firebaseio.com/"
+
+def adjust_cart(user, item_id, adjustment):
+    fb = firebase.FirebaseApplication(db_url, None)
+    cart_path = "Users/" + user + "/Cart"
+    cart_item_path = cart_path + "/" + item_id
+    item_path = "Cowbell/" + item_id
+
+    new_price = fb.get(item_path, "price") * adjustment
+    fb.put(cart_path, "cartPrice", round(fb.get(cart_path, "cartPrice") + new_price, 2))
+    new_weight = fb.get(item_path, "weight") * adjustment
+    fb.put(cart_path, "cartWeight", round(fb.get(cart_path, "cartWeight") + new_weight, 2))
+    fb.put(cart_path, "numItems", fb.get(cart_path, "numItems") + adjustment)
+
+    item = fb.get(cart_path, item_id)
+    if item == None:
+        new_item = {
+            "totalItemPrice": round(new_price, 2),
+            "totalItemWeight": round(new_weight, 2),
+            "quantity": adjustment
+        }
+        new_item.update(fb.get(item_path, None))
+        new_item.pop("stockNumber")
+        name = fb.put(cart_path, item_id, new_item)
+    else:
+        fb.put(cart_item_path, "quantity", fb.get(cart_item_path, "quantity") + adjustment)
+        fb.put(cart_item_path, "totalItemPrice", round(fb.get(cart_item_path, "totalItemPrice") + new_price, 2))
+        fb.put(cart_item_path, "totalItemWeight", round(fb.get(cart_item_path, "totalItemWeight") + new_weight, 2))
+
+    if fb.get(cart_item_path, "quantity") == 0:
+        fb.delete(cart_path, item_id)
+
+class Cowbell:
+    def add_item(self, user, item_id, quantity):
+        fb = firebase.FirebaseApplication(db_url, None)
+        item_path = "Cowbell/" + item_id
+
+        stock = fb.get(item_path, "stockNumber")
+        if (stock == None):
+            return -1
+        if (stock == 0):
+            return 0
+
+        if (stock >= quantity):
+            added_items = quantity
+            fb.put(item_path, "stockNumber", stock - quantity)
+        else:
+            added_items = stock
+            fb.put(item_path, "stockNumber", 0)
+        adjust_cart(user, item_id, added_items)
+
+        return added_items
+    
+    def remove_item(self, user, item_id, quantity):
+        fb = firebase.FirebaseApplication(db_url, None)
+        item_path = "Cowbell/" + item_id
+        cart_path = "Users/" + user + "/Cart"
+        cart_item_path = cart_path + "/" + item_id
+        
+        num_in_cart = fb.get(cart_item_path, "quantity")
+        if (num_in_cart == None):
+            return -1
+        if (num_in_cart >= quantity):
+            removed_items = quantity
+        else: 
+            removed_items = num_in_cart
+        adjust_cart(user, item_id, -1 * removed_items)
+        
+        fb.put(item_path, "stockNumber", fb.get(item_path, "stockNumber") + removed_items)
+
+        return removed_items
+
+    def display(self):
+        fb = firebase.FirebaseApplication(db_url, None)
+        category_path = "Cowbell/"
+
+        items = fb.get(category_path, None)
+        for key, value in items.items():
+            print(key, value)
---
+++ final_version/store_screen.py
@@ -0,0 +1,137 @@
+from firebase import firebase
+from user import User
+from shirt import Shirt
+from jacket import Jacket
+from hat import Hat
+from cowbell import Cowbell
+
+db_url = "https://groupproject-8d2e6-default-rtdb.firebaseio.com/"
+fb = firebase.FirebaseApplication(db_url, None)
+
+def store_screen(user, command):
+    if (command == "help"):
+        help()
+    elif (command == "display shirts"):
+        shirt = Shirt()
+        shirt.display()
+    elif (command == "display jackets"):
+        jacket = Jacket()
+        jacket.display()
+    elif (command == "display hats"):
+        hat = Hat()
+        hat.display()
+    elif (command == "display cowbells"):
+        cowbell = Cowbell()
+        cowbell.display()
+    elif (command == "edit payment"):
+        user.edit_payment()
+    elif (command == "edit shipping"):
+        user.edit_shipping()
+    elif (command == "add item"):
+        add_item(user)
+    elif (command == "remove item"):
+        remove_item(user)
+    elif (command == "checkout"):
+        checkout(user)
+    elif (command == "display cart"):
+        display_cart(user)
+    elif (command == "display orders"):
+        display_orders(user)
+    elif (command == "delete account"):
+        delete_account(user)
+    elif (command == "logout"):
+        print("Logging out")
+        user.clear_cart()
+        user.logged_in = False
+    else:
+        print("Invalid command")
+    print()
+
+def help():
+    print("'display <shirts/jackets/hats/cowbells>' : Displays all items in the specified category")
+    print("'display orders'                         : Displays order history")
+    print("'display cart'                           : Displays cart")
+    print("'edit <payment/shipping>'                : Edit payment or shipping information")
+    print("'add item'                               : Add an item to your cart")
+    print("'remove item'                            : Remove an item from your cart")
+    print("'checkout'                               : Checkout with the current items in the cart")
+    print("'logout'                                 : Logout of your account")
+    print("'delete account'                         : Delete your account")
+    print("'help'                                   : Display this menu")
+
+def add_item(user):
+    valid_input = False
+
+    item_id = input("Enter the item id of the item you want to add: ")
+    # Loop until positive integer is entered
+    while not valid_input:
+        quantity = input("Enter the quantity of the item id (positive integer): ")
+        try:
+            quantity = int(quantity)
+            if quantity > 0:
+                valid_input = True
+        except:
+            pass
+    
+    added_items = user.add_to_cart(item_id, quantity)
+    if (added_items == -1):
+        print("Item does not exist")
+    elif (added_items == 0):
+        print("Item is out of stock")
+    elif (added_items == quantity):
+        print("Added the items to your cart")
+    else:
+        print("Not enough stock to add all requested items. Added " + str(added_items) + " items")
+
+def remove_item(user):
+    valid_input = False
+
+    item_id = input("Enter the item id of the item you want to remove: ")
+    # Loop until positive integer is entered
+    while not valid_input:
+        quantity = input("Enter the quantity of the item id (positive integer): ")
+        try:
+            quantity = int(quantity)
+            if quantity > 0:
+                valid_input = True
+        except:
+            pass
+    
+    removed_items = user.remove_from_cart(item_id, quantity)
+    if (removed_items == -1 or removed_items == 0):
+        print("Item not in cart")
+    elif (removed_items == quantity):
+        print("Removed the items from your cart")
+    else:
+        print("You tried to remove more items than were in your cart. Removed all instances of that item in your cart")
+
+def checkout(user):
+    val = user.checkout()
+    if val == -1:
+        print("No items in cart!")
+    print("Transaction complete!")
+
+def display_cart(user):
+    val = user.display_cart()
+    if val == -1:
+        print("Cart has no items inside")
+
+def display_orders(user):
+    val = user.display_orders()
+    if val == -1:
+        print("No order history")
+
+def delete_account(user):
+    confirm = input("Are you sure (y/n): ")
+    if (confirm != "y"):
+        print("Account deletion cancelled")
+        return -1
+    
+    # Clear cart and restock inventory
+    user.clear_cart()
+
+    # Delete account from database
+    fb.delete("Users", user.username)
+
+    # Exit from store screen upon deletion
+    user.logged_in = False
